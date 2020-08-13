31cb93d19f6dbfeccae945fc4a1b40b4ad7e5166
umairarshadbutt <umair6622@gmail.com>
2020-08-14 03:53:38 +0500

    Linters integration with fixing issues

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-    belongs_to :post
-    belongs_to :user
-    validates :commenter, presence: true, length: { minimum: 4 }
-    validates :text, presence: true, length: { minimum: 6 }
+  belongs_to :post
+  belongs_to :user
+  validates :commenter, presence: true, length: { minimum: 4 }
+  validates :text, presence: true, length: { minimum: 6 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-
-    has_many :posts
-    has_many :comments
-    validates :name, presence: true, uniqueness: true,length: { minimum: 3 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  has_many :posts
+  has_many :comments
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
