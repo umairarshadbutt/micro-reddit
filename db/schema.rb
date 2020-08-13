1eb1f282b55f3b8b1ce3af7e052187ff5f2a87f6
umairarshadbutt <umair6622@gmail.com>
2020-08-14 03:43:43 +0500

    Add comment section

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_204550) do
+ActiveRecord::Schema.define(version: 2020_08_13_221545) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.string "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -28,5 +37,6 @@ ActiveRecord::Schema.define(version: 2020_08_13_204550) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "users"
 end
