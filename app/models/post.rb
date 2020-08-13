d1596cf867a808050f48969f21cea1f6b51f4fc0
umairarshadbutt <umair6622@gmail.com>
2020-08-14 01:56:56 +0500

    Add association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
+    belongs_to :users
     validates :title, presence: true, length: { minimum: 7 }
     validates :text, presence: true, length: { minimum: 30 }
-    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_191955) do
+ActiveRecord::Schema.define(version: 2020_08_13_204550) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,4 +28,5 @@ ActiveRecord::Schema.define(version: 2020_08_13_191955) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "users"
 end
