9cbf1f31f7f74461f5591672b000cb35bfcbf2d9
umairarshadbutt <umair6622@gmail.com>
2020-08-15 00:26:37 +0500

    Required Change: comments belong to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_221545) do
+ActiveRecord::Schema.define(version: 2020_08_14_192450) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -18,7 +18,9 @@ ActiveRecord::Schema.define(version: 2020_08_13_221545) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "post_id", null: false
+    t.integer "user_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -38,5 +40,6 @@ ActiveRecord::Schema.define(version: 2020_08_13_221545) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
